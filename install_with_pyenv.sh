1be84ef720c76a63e92f3dc46448e77a6f7ae1d7
stophittingyoursxlf <romashenko.y@yandex.ru>
2025-08-31 23:07:34 +0300

    Update install_with_pyenv.sh

--- install_with_pyenv.sh
+++ install_with_pyenv.sh
@@ -4,6 +4,7 @@ set -e
 
 echo "Устанавливаем pyenv и Python 3.12.3..."
 
+echo "Устанавливаем зависимости..."
 sudo apt-get update
 sudo apt-get install -y make build-essential libssl-dev zlib1g-dev \
 libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
@@ -11,7 +12,7 @@ libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-d
 
 if [ ! -d "$HOME/.pyenv" ]; then
     echo "Устанавливаем pyenv..."
-    curl https://pyenv.run | bash
+    curl -fsSL https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
 else
     echo "pyenv уже установлен"
 fi
@@ -21,7 +22,16 @@ if ! grep -q "pyenv" ~/.bashrc; then
     echo 'export PATH="$HOME/.pyenv/bin:$PATH"' >> ~/.bashrc
     echo 'eval "$(pyenv init -)"' >> ~/.bashrc
     echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
+fi
+
+export PATH="$HOME/.pyenv/bin:$PATH"
+eval "$(pyenv init -)"
+
+if ! command -v pyenv &> /dev/null; then
+    echo "pyenv не доступен. Пробуем обновить сессию..."
     source ~/.bashrc
+    export PATH="$HOME/.pyenv/bin:$PATH"
+    eval "$(pyenv init -)"
 fi
 
 echo "Устанавливаем Python 3.12.3..."
